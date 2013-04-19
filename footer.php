15bca20d9736e59bb07bbc752e026b3119777a99
Weldan Jamili <mweldan@gmail.com>
2013-04-20 01:18:04 +0800

    initial commit, converted html to bare wordpress theme.

---
+++ footer.php
@@ -0,0 +1,121 @@
+<div id="footer_area" class="full_width">
+<div class="page">
+<div id="prefootx">
+    <div class="preFootn-container">
+	<div class="preFootn-title-container">
+	    <h3 class="preFootn-title">About us</h3>
+	    <h3 class="preFootn-title">Resources</h3>
+	    <h3 class="preFootn-title">Other projects</h3>
+	    <h3 class="preFootn-title" id="fourth">Newsletter</h3>
+	</div>
+	<div class="preFootn" id="first">
+	    <div class="textpreFootn">
+		ProductiveMuslim.com is a brand that inspires young Muslims to become Productive through Islam and applying the latest productivity techniques.
+		<br /><br />
+		Join us, Discuss with us, and share with us your thoughts, let us all work together towards a Productive Ummah!
+	    </div>
+	</div><!-- #first .preFootn-area -->
+	<div class="preFootn" id="second">
+	    <ul>
+		<li><a target="_top" href="/the-daily-taskinator/">Daily Taskinator</a>: Your one-sheet hack to getting tasks done.</li>
+		<li><a target="_top" href="/the-weekly-taskinator">Weekly Taskinator</a>: Know your projects each day of the week. </li>
+		<li><a target="_top" href="/the-productivemuslim-habitator">ProductiveMuslim Habitator</a>: Build good habits or destroy them with our habitator.</li>
+		<li><a target="_top" href="/category/resources/wallpapers">ProductiveMuslim Wallpaper</a>: Inspire yourself with our wallpaper design.</li>
+	    </ul>
+	</div><!-- #second .preFootn-area -->
+	<div class="preFootn" id="third">
+	    <ul>
+		<li><a target="_top" href="http://www.productiveramadan.com/">Productive Ramadan</a></li>
+		<li><a target="_top" href="http://www.1000gooddeeds.com/">1000 Good Deeds</a></li>
+	    </ul>
+	</div><!-- #third .preFootn-area -->
+	<div class="preFootn" id="fourth">
+	    <!-- OptinSkin -->
+	<div class="ois_wrapper " data="4"data-popup-scroll="100px"style="" id="ois_4" rel="homepage"><div class="footerwidget">
+<form action="http://www.aweber.com/scripts/addlead.pl" method="post" id="ois_1_form">
+
+<input type="hidden" name="meta_web_form_id" value="1295039521" />
+<input type="hidden" name="meta_split_id" value="" />
+<input type="hidden" name="listname" value="prom-newsletter" />
+<input type="hidden" name="redirect" value="http://www.productivemuslim.com/check-your-email/" id="redirect_761be8407ff3d734133d6afa8840bf7e" />
+<input type="hidden" name="meta_redirect_onlist" value="http://www.productivemuslim.com/thanks-for-signing-up-for-email-updates/" />
+<input type="hidden" name="meta_adtracking" value="Footer" />
+<input type="hidden" name="meta_message" value="1001" />
+<input type="hidden" name="meta_required" value="name" />
+<input type="hidden" name="meta_required" value="email" />
+<input type="hidden" name="meta_tooltip" value="" />
+
+	Subscribe to our e-PRO newsletter for exclusive productivity content:
+	<br /><br />
+	<div align="center">
+        <input type="text" name="name" class="inputemail" placeholder="Full Name" value=""><br />
+	<input id="awf_field-36046023" type="text" name="email" value="" class="inputemail" placeholder="Email Address"  />
+	<br />
+	<button name="submit" type="submit">JOIN NOW</button>
+        <br /><br />
+	<img src="http://forms.aweber.com/form/ci/?tc=ffffff&bg=c66617&d=TMyszByMbD6C6qZGpk4%2BzKycHMzsLKw%3D" alt="Subscriber Counter" />
+	</div>
+</form>
+</div></div>
+		<!-- End OptinSkin -->	</div><!-- #fourth .preFootn-area -->
+    </div><!-- #preFootn-container -->
+    <div id="footerEndn">
+        <div id="footerEndSocial">
+	    <a target="_top" href="https://plus.google.com/116458704759826710405/posts" onClick="_gaq.push(['_trackEvent','G+Button-Footer','Visited G+ Page from Footer','Widget-Footer-StayInTouch',,false]);"><img alt="ProductiveMuslim Google Plus" src="http://www.productivemuslim.com/wp-content/uploads/2012/04/googleplus.png"></a>
+	    <a target="_top" href="http://www.facebook.com/ProductiveMuslim" onClick="_gaq.push(['_trackEvent','FaceBook-Button-Footer','Visited Facebook Page from Footer','Widget-Footer-StayInTouch',,false]);"><img alt="ProductiveMuslim Facebook" src="http://www.productivemuslim.com/wp-content/uploads/2012/04/facebook.png"></a>
+	    <a target="_top" href="http://twitter.com/AbuProductive" onClick="_gaq.push(['_trackEvent','Twitter-Button-Footer','Visited Twitter Page from Footer','Widget-Footer-StayInTouch',,false]);"><img alt="ProductiveMuslim Twitter" src="http://www.productivemuslim.com//wp-content/uploads/2012/04/twitter.png"></a>
+	    <a target="_top" href="http://www.youtube.com/ProductiveMuslim" onClick="_gaq.push(['_trackEvent','YouTube-Button-Footer','Visited YouTube Page from Footer','Widget-Footer-StayInTouch',,false]);"><img alt="ProductiveMuslim Youtube" src="http://www.productivemuslim.com/wp-content/uploads/2012/04/youtube.png"></a>
+	    <a target="_top" href="http://feeds.feedburner.com/ProductiveMuslim" onClick="_gaq.push(['_trackEvent','RSSfeed-Button-Footer','Visited RSS Feed Page from Footer','Widget-Footer-StayInTouch',,false]);"><img alt="ProductiveMuslim RSS" src="http://www.productivemuslim.com/wp-content/uploads/2012/04/rss.png"></a>
+	</div>
+	<div id="footerEndCopyright">
+	    <a href="/about/">About</a> | <a href="/site-map/">Site Map</a> | <a href="/privacy-policy/">Privacy Policy</a> |  <a href="/terms-of-service/">Terms of Service</a> |  <a href="/refund-policy/">Refund Policy</a> | <a href="/contact-http://productivemuslim.com/wp-admin/plugins.php?page=cloudflareus">Contact</a> | &copy; 2008-2013 ProductiveMuslim.com
+	</div>
+    </div><!-- #footerEnd -->
+</div><!-- #prefooterx -->	<div id="footer">
+		<p>Get smart with the <a href="http://diythemes.com/thesis/">Thesis WordPress Theme</a> from DIYthemes.</p>
+		<p><a href="http://productivemuslim.com/wp-admin/">WordPress Admin</a></p>
+<script type="text/javascript">
+jQuery(document).ready(function(){
+  var q2w3_sidebar_options = { "sidebar" : "q2w3_default", "margin_top" : 10, "margin_bottom" : 0, "widgets" : ['text-9'] }
+  setInterval(function () { q2w3_sidebar(q2w3_sidebar_options); }, 1000);
+});
+</script>
+	<div style="display:none">
+	</div>
+<link rel='stylesheet' id='digg-digg-css'  href='http://productivemuslim.com/wp-content/plugins/digg-digg/css/diggdigg-style.css?ver=5.3.3' type='text/css' media='screen' />
+<script type='text/javascript' src='http://s0.wp.com/wp-content/js/devicepx-jetpack.js?ver=201316'></script>
+<script type='text/javascript' src='http://s.gravatar.com/js/gprofiles.js?ver=2013Apraa'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var WPGroHo = {"my_hash":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://productivemuslim.com/wp-content/plugins/jetpack/modules/wpgroho.js?ver=3.5.1'></script>
+    <!-- BEGIN Aweber Script -->
+	<script type="text/javascript" src="http://analytics.aweber.com/js/awt_analytics.js?id=8CEz"></script>
+    <!-- END Aweber Script -->    <!-- BEGIN Tynt Script -->
+    <script type="text/javascript">
+    if(document.location.protocol=='http:'){
+     var Tynt=Tynt||[];Tynt.push('aAKb0q96ir4AVFacwqm_6r');Tynt.i={"ap":"Read more:"};
+     (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
+     /** Tynt SpeedShare */
+     (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/tss.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
+    }
+    </script>
+    <!-- END Tynt Script -->
+	<script src="http://stats.wordpress.com/e-201316.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	st_go({v:'ext',j:'1:2.2.2',blog:'38978169',post:'0',tz:'0'});
+	var load_cmc = function(){linktracker_init(38978169,0,2);};
+	if ( typeof addLoadEvent != 'undefined' ) addLoadEvent(load_cmc);
+	else load_cmc();
+	</script>	</div>
+<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
+</div>
+</div>
+<!--[if lte IE 8]>
+<div id="ie_clear"></div>
+<![endif]-->
+<script type="text/javascript">if(!NREUMQ.f){NREUMQ.f=function(){NREUMQ.push(["load",new Date().getTime()]);var e=document.createElement("script");e.type="text/javascript";e.src=(("http:"===document.location.protocol)?"http:":"https:")+"//"+"d1ros97qkrwjf5.cloudfront.net/42/eum/rum.js";document.body.appendChild(e);if(NREUMQ.a)NREUMQ.a();};NREUMQ.a=window.onload;window.onload=NREUMQ.f;};NREUMQ.push(["nrfj","beacon-3.newrelic.com","150aa0dc3e","2169313","NgRUbBcFXEVTVRFdXw9Od1sRDV1YHV8LUFUZ",0,2445,new Date().getTime(),"","","","",""]);</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+get_header();
+?>
+<div id="content_area" class="full_width">
+<div class="page">
+	<div id="content_box">
+		<div id="content" class="hfeed">
+
+<div id="feature_box">
+<ul class="sidebar_list"></ul></div>
+			<div class="post-5716 post type-post status-publish format-standard hentry category-islam-productivity tag-focus tag-wasting-time post_box top" id="post-5716">
+				<div class="headline_area">
+					<h2 class="entry-title"><a href="http://productivemuslim.com/youtube-culture-and-productivity-part-1/" rel="bookmark" title="Permanent link to The YouTube Culture and Your Productivity (Part 1)">The YouTube Culture and Your Productivity (Part 1)</a></h2>
+					<p class="headline_meta">by <span class="author vcard fn">Maryam Mujahid</span> on <abbr class="published" title="2013-04-15">April 15, 2013</abbr>		<div class="metaInfo">
+			<p class="author"><em>by</em> Maryam Mujahid<span class="to_comments"><a href="http://productivemuslim.com/youtube-culture-and-productivity-part-1/#respond" title="Comment on The YouTube Culture and Your Productivity (Part 1)">No Comments</a></span></p>
+			<p>Posted on Monday, April 15, 2013 in <a href="http://productivemuslim.com/category/topics/islam-productivity/" title="View all posts in Islam &amp; Productivity" rel="category tag">Islam &amp; Productivity</a></p>
+			               </div><!-- metaInfo -->
+</p>
+				</div>
+				<div class="format_text entry-content">
+<p><em><a href="http://productivemuslim.com/youtube-culture-and-productivity-part-1" ><img class="alignleft size-full wp-image-5718" title="The YouTube Culture and Your Productivity (Part 1) - Productive Muslim" alt="The YouTube Culture and Your Productivity (Part 1) - Productive Muslim" src="http://productivemuslim.com/wp-content/uploads/2013/04/04-13-youtube-300px.png" width="300" height="225" /></a>In the first part of this series, you will read about the YouTube generation, information overload, and what you can do to manage the incessant stream of information and stay focused and productive.</em></p>
+<h2>Generation YouTube</h2>
+<p>In case you still had any doubt about it, YouTube has conquered the Internet, our lives and our time, in a matter of what seemed to be just a few years. At the same time, it has also given a whole new dimension to sharing information in general, and information about our deen in particular. Getting basic know-how or deeper knowledge about Islam has never been easier or faster. There are literally thousands of videos being uploaded every minute all around the world, from recorded lectures of well-known sheikhs, to enthusiastic individuals vlogging their two cents into cyberspace. <a href="http://productivemuslim.com/youtube-culture-and-productivity-part-1/#more-5716"  class="more-link">[Click to Continue&hellip;]</a></p>
+				</div>
+			</div>
+
+			<div class="post-5703 post type-post status-publish format-standard hentry category-get-motivated post_box" id="post-5703">
+				<div class="headline_area">
+					<h2 class="entry-title"><a href="http://productivemuslim.com/productive-parties-for-children/" rel="bookmark" title="Permanent link to Productive Parties for Children">Productive Parties for Children</a></h2>
+					<p class="headline_meta">by <span class="author vcard fn">Sana Gul</span> on <abbr class="published" title="2013-04-12">April 12, 2013</abbr>		<div class="metaInfo">
+			<p class="author"><em>by</em> Sana Gul<span class="to_comments"><a href="http://productivemuslim.com/productive-parties-for-children/#comments" title="Comment on Productive Parties for Children">8 Comments</a></span></p>
+			<p>Posted on Friday, April 12, 2013 in <a href="http://productivemuslim.com/category/topics/get-motivated/" title="View all posts in Get Motivated" rel="category tag">Get Motivated</a></p>
+			               </div><!-- metaInfo -->
+</p>
+				</div>
+				<div class="format_text entry-content">
+<p><a href="http://productivemuslim.com/productive-parties-for-children" ><img class="alignleft size-full wp-image-5706" title="Productive Parties for Children - Productive Muslim" alt="Productive Parties for Children - Productive Muslim" src="http://productivemuslim.com/wp-content/uploads/2013/04/04-13-parties-300px.png" width="300" height="225" /></a>The Messenger of Allah <img title="ṣallallāhu 'alayhi wa sallam (peace and blessings of Allāh be upon him)" alt="ṣallallāhu 'alayhi wa sallam (peace and blessings of Allāh be upon him)" class="islamic_graphic" src="http://productivemuslim.com/wp-content/plugins/islamic-graphics/img/black/20/saw.png" height="20px"><span class="islamic_graphic"> (peace and blessings of Allāh be upon him)</span> said, &#8220;When a group of people assemble for the remembrance of Allah <img title="subḥānahu wa ta'āla (glorified and exalted be He)" alt="subḥānahu wa ta'āla (glorified and exalted be He)" class="islamic_graphic" src="http://productivemuslim.com/wp-content/plugins/islamic-graphics/img/black/20/swt.png" height="20px"><span class="islamic_graphic"> (glorified and exalted be He)</span>, the angels surround them (with their wings), (Allah&#8217;s) mercy envelops them, Sakinah or tranquility descends upon them and Allah makes a mention of them before those who are near Him.&#8221;[<a title="Riyad as-Salihin &gt; The Book of the Remembrance of Allah" href="http://www.sunnah.com/riyadussaliheen/16#41" onclick="javascript:_gaq.push(['_trackEvent','outbound-article','http://www.sunnah.com/riyadussaliheen/16#41']);" target="_blank">Sahih Muslim</a>].</p>
+<p>The purpose of life in this dunya is to remember Allah <img title="subḥānahu wa ta'āla (glorified and exalted be He)" alt="subḥānahu wa ta'āla (glorified and exalted be He)" class="islamic_graphic" src="http://productivemuslim.com/wp-content/plugins/islamic-graphics/img/black/20/swt.png" height="20px"><span class="islamic_graphic"> (glorified and exalted be He)</span> at all times. And the Sunnah of the Messenger of Allah <img title="ṣallallāhu 'alayhi wa sallam (peace and blessings of Allāh be upon him)" alt="ṣallallāhu 'alayhi wa sallam (peace and blessings of Allāh be upon him)" class="islamic_graphic" src="http://productivemuslim.com/wp-content/plugins/islamic-graphics/img/black/20/saw.png" height="20px"><span class="islamic_graphic"> (peace and blessings of Allāh be upon him)</span> teaches us how to do just that. Our children love to have parties as much as we do; such as play dates or Eid parties or a celebration for some accomplishment they&#8217;ve attained.</p>
+<p>These parties are the perfect opportunity to instil in our children the habit of always remembering Allah <img title="subḥānahu wa ta'āla (glorified and exalted be He)" alt="subḥānahu wa ta'āla (glorified and exalted be He)" class="islamic_graphic" src="http://productivemuslim.com/wp-content/plugins/islamic-graphics/img/black/20/swt.png" height="20px"><span class="islamic_graphic"> (glorified and exalted be He)</span> in any gathering they attend. The idea of angels descending and surrounding them with their wings is enough to put wonder in their hearts and the fact that Allah <img title="subḥānahu wa ta'āla (glorified and exalted be He)" alt="subḥānahu wa ta'āla (glorified and exalted be He)" class="islamic_graphic" src="http://productivemuslim.com/wp-content/plugins/islamic-graphics/img/black/20/swt.png" height="20px"><span class="islamic_graphic"> (glorified and exalted be He)</span> is going to mention them to His angels makes them walk happily around for quite some time!! <a href="http://productivemuslim.com/productive-parties-for-children/#more-5703"  class="more-link">[Click to Continue&hellip;]</a></p>
+				</div>
+			</div>
+
+			<div class="post-5692 post type-post status-publish format-standard hentry category-interviews category-islam-productivity tag-health tag-nutrition post_box" id="post-5692">
+				<div class="headline_area">
+					<h2 class="entry-title"><a href="http://productivemuslim.com/interview-nourish-your-way-to-a-productive-lifestyle/" rel="bookmark" title="Permanent link to [Interview] Nourish Your Way to a Productive Lifestyle">[Interview] Nourish Your Way to a Productive Lifestyle</a></h2>
+					<p class="headline_meta">by <span class="author vcard fn">Abu Productive</span> on <abbr class="published" title="2013-04-10">April 10, 2013</abbr>		<div class="metaInfo">
+			<p class="author"><em>by</em> Abu Productive<span class="to_comments"><a href="http://productivemuslim.com/interview-nourish-your-way-to-a-productive-lifestyle/#comments" title="Comment on [Interview] Nourish Your Way to a Productive Lifestyle">9 Comments</a></span></p>
+			<p>Posted on Wednesday, April 10, 2013 in <a href="http://productivemuslim.com/category/interviews/" title="View all posts in Interviews" rel="category tag">Interviews</a>, <a href="http://productivemuslim.com/category/topics/islam-productivity/" title="View all posts in Islam &amp; Productivity" rel="category tag">Islam &amp; Productivity</a></p>
+			               </div><!-- metaInfo -->
+</p>
+				</div>
+				<div class="format_text entry-content">
+<p><a href="http://productivemuslim.com/interview-nourish-your-way-to-a-productive-lifestyle" ><img class="size-medium wp-image-5698 alignleft" title="[Interview] Nourish Your Way to a Productive Lifestyle - Productive Muslim" alt="[Interview] Nourish Your Way to a Productive Lifestyle - Productive Muslim" src="http://productivemuslim.com/wp-content/uploads/2013/04/Nourition-1360321362_6001-300x111.jpg" width="300" height="111" /></a>Nour Zibdeh is a Registered Dietitian Nutritionist (RDN), Certified LEAP Therapist (CLT), and the owner of Nourition, LLC, a nutrition counseling practice. Nour specializes in nutrition management for digestive conditions (irritable bowel syndrome, inflammatory bowel disease, celiac disease, non-celiac gluten sensitivities), food sensitivities, migraines and headaches, fibromyalgia, ADD/ADHD, eczema, autism spectrum disorders, weight management, and cardiovascular disease.</p>
+<p>In this interview we talked about the best ways to nourish ourselves for peak performance throughout the day. We hope you benefit. Please share your feedback by leaving a comment below!</p>
+<div class="powerpress_player" id="powerpress_player_4088"></div>
+<script type="text/javascript"><!--
+pp_flashembed(
+	'powerpress_player_4088',
+	{src: 'http://productivemuslim.com/wp-content/plugins/powerpress/FlowPlayerClassic.swf', width: '320', height: '24', wmode: 'transparent' },
+	{config: { autoPlay: false, autoBuffering: false, showFullScreenButton: false, showMenu: false, videoFile: 'http://dl.dropbox.com/u/5173759/Interview-with-Nour-Zibdeh-Nourition.mp3', loop: false, autoRewind: true } }
+);
+//-->
+</script>
+<p class="powerpress_links powerpress_links_mp3">Podcast: <a href="http://dl.dropbox.com/u/5173759/Interview-with-Nour-Zibdeh-Nourition.mp3"  class="powerpress_link_pinw" target="_blank" title="Play in new window" onclick="javascript:_gaq.push(['_trackEvent','download','http://dl.dropbox.com/u/5173759/Interview-with-Nour-Zibdeh-Nourition.mp3']);return powerpress_pinw("5692-podcast');" rel="nofollow">Play in new window</a>
+ | <a href="http://dl.dropbox.com/u/5173759/Interview-with-Nour-Zibdeh-Nourition.mp3" onclick="javascript:_gaq.push(['_trackEvent','download','http://dl.dropbox.com/u/5173759/Interview-with-Nour-Zibdeh-Nourition.mp3']);" class="powerpress_link_d" title="Download" rel="nofollow">Download</a> (Duration: 37:23 &#8212; 42.8MB)
+</p>
+
+				</div>
+			</div>
+
+			<div class="post-5662 post type-post status-publish format-standard hentry category-work-better tag-organization post_box" id="post-5662">
+				<div class="headline_area">
+					<h2 class="entry-title"><a href="http://productivemuslim.com/email-management/" rel="bookmark" title="Permanent link to [Productivity Ninja] Email Management &#8211; How Inbox Zero Works">[Productivity Ninja] Email Management &#8211; How Inbox Zero Works</a></h2>
+					<p class="headline_meta">by <span class="author vcard fn">Graham Allcott</span> on <abbr class="published" title="2013-04-08">April 8, 2013</abbr>		<div class="metaInfo">
+			<p class="author"><em>by</em> Graham Allcott<span class="to_comments"><a href="http://productivemuslim.com/email-management/#comments" title="Comment on [Productivity Ninja] Email Management &#8211; How Inbox Zero Works">5 Comments</a></span></p>
+			<p>Posted on Monday, April 8, 2013 in <a href="http://productivemuslim.com/category/topics/work-better/" title="View all posts in Work Better" rel="category tag">Work Better</a></p>
+			               </div><!-- metaInfo -->
+</p>
+				</div>
+				<div class="format_text entry-content">
+<p><em><a href="http://productivemuslim.com/email-management" ><img class="alignleft size-full wp-image-5664" title="[Productivity Ninja] Email Management - How Inbox Zero Works - Productive Muslim" alt="[Productivity Ninja] Email Management - How Inbox Zero Works - Productive Muslim" src="http://productivemuslim.com/wp-content/uploads/2013/04/4-email-management-image.jpg" width="236" height="249" /></a>Let’s start with the uncomfortable truth about the way that we work</em>.</p>
+<p>As soon as more than one thing has our attention and we experience information overload, our instinctive reaction is that we want to feel busy to feel like we’re making progress. Because as a species we’re inherently lazy, we gravitate to the easiest way to achieve this illusion of progress.</p>
+<p>We open our email inbox, check for what’s new, we scroll up and down, we check for what’s new, we scroll up and down, we fiddle around creating archive folders, we check for other new information (for example on our social media profiles or the news or our phone) and generally begin to develop an addiction to being connected. What we’re addicted to here is the illusion of productivity for a minimal payoff of thinking. <a href="http://productivemuslim.com/email-management/#more-5662"  class="more-link">[Click to Continue&hellip;]</a></p>
+				</div>
+			</div>
+
+			<div class="post-5646 post type-post status-publish format-standard hentry category-work-better tag-family tag-relationships post_box" id="post-5646">
+				<div class="headline_area">
+					<h2 class="entry-title"><a href="http://productivemuslim.com/striving-to-achieve-a-productive-and-peaceful-relationship-with-your-mother-in-law-part-2/" rel="bookmark" title="Permanent link to Striving to Achieve a Productive and Peaceful Relationship with Your Mother-in-Law (Part 2)">Striving to Achieve a Productive and Peaceful Relationship with Your Mother-in-Law (Part 2)</a></h2>
+					<p class="headline_meta">by <span class="author vcard fn">Umm Muhemmed</span> on <abbr class="published" title="2013-04-05">April 5, 2013</abbr>		<div class="metaInfo">
+			<p class="author"><em>by</em> Umm Muhemmed<span class="to_comments"><a href="http://productivemuslim.com/striving-to-achieve-a-productive-and-peaceful-relationship-with-your-mother-in-law-part-2/#comments" title="Comment on Striving to Achieve a Productive and Peaceful Relationship with Your Mother-in-Law (Part 2)">11 Comments</a></span></p>
+			<p>Posted on Friday, April 5, 2013 in <a href="http://productivemuslim.com/category/topics/work-better/" title="View all posts in Work Better" rel="category tag">Work Better</a></p>
+			               </div><!-- metaInfo -->
+</p>
+				</div>
+				<div class="format_text entry-content">
+<p><a href="http://productivemuslim.com/striving-to-achieve-a-productive-and-peaceful-relationship-with-your-mother-in-law-part-2" ><img class="alignleft size-full wp-image-5647" title="Striving to Achieve a Productive and Peaceful Relationship with Your Mother-in-Law (Part 2) - Productive Muslim" alt="Striving to Achieve a Productive and Peaceful Relationship with Your Mother-in-Law (Part 2) - Productive Muslim" src="http://productivemuslim.com/wp-content/uploads/2013/04/04-13-MIL-300px.png" width="300" height="225" /></a>“My mother’s coming for two months,” your spouse says, gleefully, then turns to embrace you. There is a sinking feeling, even though there shouldn’t be. All of a sudden, your sense of autonomy, purposefulness and passion are all drained. I married you, not your mother, you think to yourself, but don’t actually say it. “Inshaa Allah,” instead you respond. However, your heart tightens, and you ask God to give you the strength to endure another visit.</p>
+<p>All too often, the mother-in-law-daughter relationship is fraught with tension and resentment. Seldom do we encounter examples of these two women really thriving in their relationship. In the <a title="Striving to Achieve a Productive and Peaceful Relationship with Your Mother-in-Law (Part 1)" href="http://productivemuslim.com/striving-to-achieve-a-productive-and-peaceful-relationship-with-your-mother-in-law-part-1/"  target="_blank">first article</a> in this two-part series, I suggested ways for you to organize your kitchen and other parts of your home to improve the relationship. <a href="http://productivemuslim.com/striving-to-achieve-a-productive-and-peaceful-relationship-with-your-mother-in-law-part-2/#more-5646"  class="more-link">[Click to Continue&hellip;]</a></p>
+				</div>
+			</div>
+
+<div class='prev_next'><div class='wp-pagenavi'>
+<span class='pages'>Page 1 of 117</span><span class='current'>1</span><a href='http://productivemuslim.com/page/2/' class='page larger'>2</a><a href='http://productivemuslim.com/page/3/' class='page larger'>3</a><a href='http://productivemuslim.com/page/4/' class='page larger'>4</a><a href='http://productivemuslim.com/page/5/' class='page larger'>5</a><span class='extend'>...</span><a href='http://productivemuslim.com/page/10/' class='larger page'>10</a><a href='http://productivemuslim.com/page/20/' class='larger page'>20</a><a href='http://productivemuslim.com/page/30/' class='larger page'>30</a><span class='extend'>...</span><a href='http://productivemuslim.com/page/2/' class='nextpostslink'>»</a><a href='http://productivemuslim.com/page/117/' class='last'>Last »</a>
+</div></div>		</div>
+
+<?php 
+get_sidebar();
+get_footer();
+?>
